a60421361664a637857f9121e9dccace9898b4dd
timotdsantos <timothysantos@gmail.com>
2018-08-01 03:54:13 +0800

    Handled unbundled/imperfect integers for video

--- tribe.py
+++ tribe.py
@@ -46,10 +46,13 @@ def decompose_audio(int_n=15,i_comp=3,ii_comp=6,iii_comp=9,i_val=427.50,ii_val=8
 def decompose_video(int_n=13,i_comp=3,ii_comp=5,iii_comp=9,i_val=570,ii_val=900,iii_val=1530,media_type='VID'):
 
     out_decomposition=[]
-    for i in range(0, int_n // i_comp + 1):
-        for ii in range(0, (int_n - i_comp * i) // ii_comp + 1):
-            for iii in range(0, (int_n - i * i_comp - ii * ii_comp) // iii_comp + 1):
-                if (i * i_comp + ii * ii_comp + iii * iii_comp == int_n):
+    out_imperfect=[]
+    remainder=int_n
+
+    for i in range(0,int_n//i_comp + 1):
+        for ii in range(0,(int_n-i_comp*i)//ii_comp + 1):
+            for iii in range(0,(int_n - i*i_comp - ii*ii_comp)//iii_comp + 1):
+                if(i*i_comp + ii * ii_comp + iii * iii_comp == int_n):
                     out_decomposition.append('%d %s $%.2f' % (int_n, media_type,i * i_val + ii * ii_val + iii * iii_val))
                     if(iii>0):
                         out_decomposition.append('\t%d x %d $%d' % (iii, iii_comp, iii_val))
@@ -58,6 +61,19 @@ def decompose_video(int_n=13,i_comp=3,ii_comp=5,iii_comp=9,i_val=570,ii_val=900,
                     if(i>0):
                         out_decomposition.append('\t%d x %d $%d' % (i, i_comp, i_val))
                     return out_decomposition
+                #handle unbundled media
+                elif(int_n - (i * i_comp + ii * ii_comp + iii * iii_comp ) < remainder ):
+                    remainder = int_n - (i * i_comp + ii * ii_comp + iii * iii_comp )
+                    out_imperfect=[]
+                    out_imperfect.append('%d %s $%.2f' % (int_n, media_type,i * i_val + ii * ii_val + iii * iii_val))
+                    if(iii>0):
+                        out_imperfect.append('\t%d x %d $%d' % (iii,iii_comp, iii_val))
+                    if(ii>0):
+                        out_imperfect.append('\t%d x %d $%d' % (ii,ii_comp, ii_val))                        
+                    if(i>0):
+                        out_imperfect.append('\t%d x %d $%d' % (i,i_comp, i_val))
+                    out_imperfect.append('\t%d x 0 $0 (unbundled)' % (remainder))
+    return out_imperfect                
 
 
 ####################################################################################################
@@ -85,17 +101,17 @@ def decompose_image(int_n=10,i_comp=5,ii_comp=10,i_val=450,ii_val=800,media_type
                     out_decomposition.append('\t%d x %d $%d' % (i,i_comp, i_val))
                 return out_decomposition
             
-            elif(int_n - (i * i_comp + ii * ii_comp ) < remainder ):
-                remainder = int_n - (i * i_comp + ii * ii_comp )
-                out_imperfect=[]
-                out_imperfect.append('%d %s $%.2f' % (int_n,media_type,i*i_val+ii*ii_val))
-                if(ii>0):
-                    out_imperfect.append('\t%d x %d $%d' % (ii,ii_comp, ii_val))
-                if(i>0):
-                    out_imperfect.append('\t%d x %d $%d' % (i,i_comp, i_val))
-                out_imperfect.append('\t%d x 0 $0 (unbundled)' % (remainder))
-                
-    return out_imperfect                
+#             elif(int_n - (i * i_comp + ii * ii_comp ) < remainder ):
+#                 remainder = int_n - (i * i_comp + ii * ii_comp )
+#                 out_imperfect=[]
+#                 out_imperfect.append('%d %s $%.2f' % (int_n,media_type,i*i_val+ii*ii_val))
+#                 if(ii>0):
+#                     out_imperfect.append('\t%d x %d $%d' % (ii,ii_comp, ii_val))
+#                 if(i>0):
+#                     out_imperfect.append('\t%d x %d $%d' % (i,i_comp, i_val))
+#                 out_imperfect.append('\t%d x 0 $0 (unbundled)' % (remainder))
+#                 
+#     return out_imperfect                
 
 ####################################################################################################
 # Define extract_breakdown(). Get breakdown from one row string input
